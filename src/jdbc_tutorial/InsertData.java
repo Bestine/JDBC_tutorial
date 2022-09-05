241864c38574ed933785fbd7ece5d4e70fba25f2
Bestine <bestineokinda@gmail.com>
2022-09-05 17:23:27 +0300

    SuccessFully inserted data into a table in the database

--- src/jdbc_tutorial/InsertData.java
+++ src/jdbc_tutorial/InsertData.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class ReadDatabase {
+public class InsertData {
 	
-	ReadDatabase() {
+	InsertData(){
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sql_store",
@@ -27,6 +28,23 @@ public class ReadDatabase {
 				System.out.println(name);
 			}
 			
+			//this code inserts names into first_name column in the customers table
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Enter the first name: ");
+			String first_name = scan.nextLine();
+			
+			stmt.execute("INSERT INTO customers (first_name, last_name, address, city, state) VALUES ('"+first_name+"', 'Okinda', 'Mtopanga Posta', 'Mombasa', 'MS');");
+			System.out.println("SuccessFully Inserted " + first_name);
+			
+			ResultSet updatedNames = stmt.executeQuery("SELECT * FROM customers;");
+			
+			while(updatedNames.next()) {
+				String name = updatedNames.getString("last_name");
+				System.out.println(name);
+			}			
+			
+			stmt.close(); //closes the SQL statement
+			
 			System.out.println("CONNECTION ESTABLISHED!!!");	
 			
 		} catch (ClassNotFoundException e) {
@@ -35,6 +53,7 @@ public class ReadDatabase {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}		
 	}
+
 }
